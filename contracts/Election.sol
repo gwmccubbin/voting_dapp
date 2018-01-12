26b68522581014595126231864726b153cc4cd30
Gregory McCubbin <gwmccubbin@gmail.com>
2018-01-12 10:44:55 -0600

    Sketches out election contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -3,11 +3,38 @@ pragma solidity ^0.4.2;
 contract Election {
     string value;
 
+    struct Candidate {
+        string name;
+        uint voteCount;
+    }
+
+    // Must keep track of vote because we'll always have a default value for voters;
+    // We don't want to do an id because we'd have to keep a counter and update it;
+    struct Voter {
+        bool voted;
+    }
+
+    // How do I make this private?
+    mapping(address => Voter) public voters;
+    // Gives us access to the candidates's index publicly
+    Candidate[] public candidates;
+
+    // voted event
+
     function Election () {
         value = "Hello, world!";
     }
 
-    function getValue() public constant returns(string _value) {
-        return(value);
+
+    function Vote (uint _candidateIndex) public payable {
+        // require that they haven't voted before
+        // require a valid candidate
+
+        // mark voted true
+
+        // update candidate vote counter
+
+        // trigger voted event
     }
+
 }
