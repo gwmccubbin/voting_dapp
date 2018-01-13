f6e535e4b5b7a0f046f9e1c77fd62fe41aa6e9d8
Gregory McCubbin <gwmccubbin@gmail.com>
2018-01-13 14:29:20 -0600

    WIP Uses monolithic render function

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ App = {
   contracts: {},
   account: '0x0',
   loading: false,
+  hasVoted: false,
 
   init: function() {
     return App.initWeb3();
@@ -36,101 +37,58 @@ App = {
   },
 
   render: function() {
-    App.renderCandidatesResults();
-    App.renderCandidatesBallot();
-    App.renderAccount();
-  },
-
-  renderCandidatesResults: function() {
     var electionInstance;
+    var loader = $("#loader");
+    var content = $("#content");
 
-    App.contracts.Election.deployed().then(function(instance) {
-      electionInstance = instance;
-      return electionInstance.candidatesCount();
-    }).then(function(candidatesCount) {
-      var candidatesResults = $('#candidatesResults');
-      candidatesResults.empty();
+    loader.show();
+    content.hide();
 
-      for (var i = 1; i <= candidatesCount; i++) {
-        electionInstance.candidates(i).then(function(candidate) {
-          // Display candiate results in table
-          App.renderCandidateResult(
-            candidate[0], // id
-            candidate[1], // name
-            candidate[2], // voteCount
-          );
-        });
-      }
-      App.loading = false;
-    }).catch(function(error) {
-      App.loading = false;
-    });
-  },
-
-  renderCandidateResult: function(id, name, voteCount) {
-    console.log(id, name, voteCount)
-    // Fetch candidate list
-    var candidatesResults = $('#candidatesResults');
-
-    // Fetch candidate template & fill it in
-    var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td><tr>"
-
-    // Add candidate to the list
-    candidatesResults.append(candidateTemplate);
-  },
-
-  renderCandidatesBallot: function() {
-    var electionInstance;
-    var hasVoted;
-
-    App.contracts.Election.deployed().then(function(instance) {
-      electionInstance = instance;
-      return instance.voters(App.account);
-    }).then(function(voted) {
-      console.log("Has Voted", voted)
-      hasVoted = voted;
-      if(hasVoted) {
-        throw new Error('Candidate has already voted');
-      }
-      return electionInstance.candidatesCount();
-    }).then(function(candidatesCount) {
-      // Don't render the form if account has voted
-      var candidatesSelect = $('#candidatesSelect');
-      candidatesSelect.empty();
-
-      for (var i = 1; i <= candidatesCount; i++) {
-        electionInstance.candidates(i).then(function(candidate) {
-          // Display candidate options in select
-          App.renderCandidateOption(
-            candidate[0], // id
-            candidate[1], // name
-          );
-        });
-      }
-      App.loading = false;
-    }).catch(function(error) {
-      console.log(error.message);
-    });
-  },
-
-  renderCandidateOption: function(id, name) {
-    // Fetch candidate list
-    var candidatesSelect = $('#candidatesSelect');
-
-    // Create a candidate option
-    var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
-
-    // Add candidate selection
-    candidatesSelect.append(candidateOption);
-  },
-
-  renderAccount: function() {
-    // Get the main account on node we're connected to
+    // Load account data
     web3.eth.getCoinbase(function(err, account) {
       if (err === null) {
         App.account = account;
         $("#accountAddress").html("Your Account: " + account);
       }
+    });
+
+    // Load contract data
+    App.contracts.Election.deployed().then(function(instance) {
+      electionInstance = instance;
+      return electionInstance.candidatesCount();
+    }).then(function(candidatesCount) {
+      console.log("candidates count", candidatesCount.toNumber())
+      var candidatesResults = $("#candidatesResults");
+      candidatesResults.empty();
+
+      var candidatesSelect = $('#candidatesSelect');
+      candidatesSelect.empty();
+
+      for (var i = 1; i <= candidatesCount; i++) {
+        electionInstance.candidates(i).then(function(candidate) {
+          var id = candidate[0];
+          var name = candidate[1];
+          var voteCount = candidate[2];
+
+          // Render candidate Result
+          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
+          candidatesResults.append(candidateTemplate);
+
+          // Render candidate ballot option
+          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+          candidatesSelect.append(candidateOption);
+        });
+      }
+      return electionInstance.voters(App.account);
+    }).then(function(hasVoted) {
+      // Do not allow a user to vote
+      if(hasVoted) {
+        $('form').hide();
+      }
+      loader.hide();
+      content.show();
+    }).catch(function(error) {
+      // Handle error here...
     });
   },
 
